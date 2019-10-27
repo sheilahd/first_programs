8f94b7252c982e17d45cd8af28d142f59dcbdc2b
sheilahd <sheila.hdez@gmail.com>
2019-10-27 22:57:20 +0000

    my very first program years to hours

--- hello.rb
+++ hello.rb
@@ -1,1 +1,16 @@
-puts "Hello, Everyone!"
+
+rosa = 90
+carton = "a"
+alfre = (carton + rosa.to_s) + carton      
+a = rosa.to_s.upcase
+b = "#{a}#{rosa}"
+roxy += (rosa +roxy.to_i).to_s
+puts b + carton
+rosa = rosa.to_s + "rosa" + b
+puts variable_x
+
+
+
+
+
+
---
+++ years_to_hours.rb
@@ -0,0 +1,23 @@
+puts "Enter a number of years"
+years = gets.chomp 
+years = years.to_i
+hours = years * 365 * 24
+hours = hours.to_s
+puts "That's #{hours} hours."
+
+puts "Enter a number of decades"
+decades = gets.chomp
+decades = decades.to_i
+minutes = decades * 10 * 365 * 24 * 60
+minutes = minutes.to_s
+puts "That's #{minutes} minutes."
+
+puts "Enter your age in numbers"
+age = gets.chomp
+age = age.to_i
+seconds = age * 365 * 24 * 60 * 60
+seconds = seconds.to_s
+puts "You are #{seconds} seconds old."
+
+
+
